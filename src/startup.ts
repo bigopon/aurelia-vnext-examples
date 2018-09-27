ab0f2101b204c966e99770e05dbb91b65ab622ef
bigopon.777@gmail.com <bigopon.777@gmail.com>
2018-09-28 01:03:30 +1000

    Initial

---
+++ src/startup.ts
@@ -0,0 +1,23 @@
+import { BasicConfiguration } from '@aurelia/jit';
+import { Aurelia, ISVGAnalyzer } from '@aurelia/runtime';
+import { App } from './app';
+import { DI, IContainer } from '@aurelia/kernel';
+import { register } from '@aurelia/plugin-svg';
+import { State } from './state';
+import { Pythagoras } from './pythagoras';
+
+let state: State;
+const au = window['au'] = new Aurelia()
+  .register(
+    {
+      register(container: IContainer) {
+        state = container.get(State);
+        register(container);
+      }
+    },
+    BasicConfiguration,
+    Pythagoras as any
+  )
+  .app({ host: document.querySelector('app'), component: new App(state) });
+au
+  .start();
